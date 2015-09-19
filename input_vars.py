c12d3afeaa303e844e88a929c8d33cf5517addc1
Robert <zephyrlandroot@gmail.com>
2015-09-18 23:46:38 -0600

    Update input_vars.py
    Added a long-planned custom feature! You can now specify your own custom terrain arguments without editing the code!

--- input_vars.py
+++ input_vars.py
@@ -3,22 +3,24 @@ from math import sin
 from presets import *
 
 
-
+custom_chosen = False
 global chosen_array
 
 terrain_presets = [original1, original2, original3, plains, mountains, random_terrain, 
 downslopes, upslopes, iceplains, hills, volcanism, spires, rollinghills, pinkhills, bloodspires, 
-mesa]
+mesa, sinewave, purple_mountains, slight_hills]
 
 
 def start_up():
-	choose_option = str(input(" Choose an option. Valid options are 'Generate' and 'Help':\n  "))
+	choose_option = str(input(" Choose an option. Valid options are 'Generate', 'Custom', and 'Help':\n  "))
 	choose_option = choose_option.lower()
 	if choose_option == "generate":
 		beginGeneration()
 	elif choose_option == "help":
 		print("\n Here is a list of terrain presets:")
 		print(terrain_presets)
+	elif choose_option == "custom":
+		beginCustom()
 
 
 
@@ -30,24 +32,46 @@ def start_up():
 
 
 def beginGeneration():
-	global chosen_array
+	global chosen_array, custom_chosen
 	
-	chosen_array = input("  Enter a terrain preset ID.\n\n   Valid IDs are from 0 to " + str(len(terrain_presets)-1) + ": ")
-	try:
-		int(chosen_array)
-	except:
-		print("Invalid ID: Enter an integer.")
-		beginGeneration()
+	if custom_chosen == False:
+		chosen_array = input("  Enter a terrain preset ID.\n\n   Valid IDs are from 0 to " + str(len(terrain_presets)-1) + ": ")
+		try:
+			int(chosen_array)
+		except:
+			print("Invalid ID: Enter an integer.")
+			beginGeneration()
 	
-	chosen_array = int(chosen_array)
-	if chosen_array > len(terrain_presets)-1:
-		print("Invalid ID: Number too high.\n")
-		beginGeneration()
-	elif chosen_array < 0:
-		print("Invalid ID: Number too low.\n")
-		beginGeneration()
+		chosen_array = int(chosen_array)
+		if chosen_array > len(terrain_presets)-1:
+			print("Invalid ID: Number too high.\n")
+			beginGeneration()
+		elif chosen_array < 0:
+			print("Invalid ID: Number too low.\n")
+			beginGeneration()
+		else:
+			chosen_array = terrain_presets[chosen_array]
 	else:
-		chosen_array = terrain_presets[chosen_array]
+		chosen_array = chosen_array
+
+
+
+def beginCustom():
+	custom_terrain_intensity = int(input(" Enter an intensity: "))
+	custom_terrain_height = int(input(" Enter a height: "))
+	custom_terrain_scalex = int(input(" Enter a scale x: "))
+	custom_terrain_engine = int(input(" Enter an ID of a terrain generation engine: "))
+	custom_terrain_R = int(input(" Enter an RGB red value: "))
+	custom_terrain_G = int(input(" Enter an RGB green value: "))
+	custom_terrain_B = int(input(" Enter a RGB blue value: "))
+	custom_terrain_coloursharpness = int(input(" Enter a colour sharpness: "))
+	global chosen_array
+	chosen_array = [custom_terrain_intensity, custom_terrain_height, custom_terrain_scalex, custom_terrain_engine,
+	custom_terrain_R, custom_terrain_G, custom_terrain_B, custom_terrain_coloursharpness]
+	global custom_chosen
+	custom_chosen = True
+	beginGeneration()
+
 
 
 start_up()
